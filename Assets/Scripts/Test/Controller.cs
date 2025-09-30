65cc56b6f2de0c5c7a7f5a44f6c1466d9db2857a
dennisbov <dennis.bovyrin@gmail.com>
2025-09-30 20:53:07 +0300

    tile selector visuals implemented

--- Assets/Scripts/Test/Controller.cs
+++ Assets/Scripts/Test/Controller.cs
@@ -8,14 +8,16 @@ public class Controller : MonoBehaviour
     [SerializeField] private Camera _camera;
     [SerializeField] private TestMovement _testObject;
     [SerializeField] private LayerMask _planetLayer;
+
+    private Transform _currentTile;
     private void Update()
     {
-        if (Input.GetMouseButtonDown(1)) 
-        { 
-            if(Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, float.PositiveInfinity, _planetLayer))
-            {
-                _testObject.AddTarget(hit.point);
-            }
+        if(_currentTile != null)
+            _currentTile.gameObject.SetActive(false);
+        if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, float.PositiveInfinity, _planetLayer))
+        {
+                _currentTile = hit.transform.GetChild(0);
+                _currentTile.gameObject.SetActive(true);
         }
     }
 }
---
+++ Assets/Scripts/Test/instantScripts/CreateFrames.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CreateFrames : MonoBehaviour
+{
+    [SerializeField] private bool _createFrames = false;
+    [SerializeField] private bool _onlyDestroy;
+    [SerializeField] private GameObject _hexagonFrame;
+    [SerializeField] private GameObject _pentagonFrame;
+    [SerializeField] private List<GameObject> _pentagons = new List<GameObject>();
+    [SerializeField] private Transform _planet;
+    [SerializeField] private float _frameScaleOffset;
+    [SerializeField] private GameObject _testSphere;
+
+
+    private void OnValidate()
+    {
+        if (_createFrames)
+        {
+            foreach (Transform _tile in _planet)
+            {
+                if (_tile.childCount > 0)
+                    _tile.GetChild(0).gameObject.SetActive(false);
+            }
+            if (_onlyDestroy)
+                return;
+            foreach (Transform tile in _planet)
+            {
+                GameObject frame;
+                if (_pentagons.Contains(tile.gameObject))
+                    frame = Instantiate(_pentagonFrame);
+                else
+                    frame = Instantiate(_hexagonFrame);
+                Mesh mesh = tile.GetComponent<MeshFilter>().mesh;
+
+                Vector3 center = new Vector3();
+                Vector3 thirdVertex = new Vector3();   
+                foreach(Vector3 vertice in mesh.vertices)
+                {
+                    center += vertice;
+                    if (vertice != mesh.vertices[0] && mesh.vertices[0]-center != -(vertice-center))
+                        thirdVertex = vertice;
+                }
+                center = center / mesh.vertices.Length;
+                float scale = (mesh.vertices[0] - center).magnitude * tile.lossyScale.x;
+                frame.transform.position = center * tile.lossyScale.x;
+                Plane plane = new Plane(
+                    center * tile.lossyScale.x * 100f, 
+                    mesh.vertices[0] * tile.lossyScale.x * 100f, 
+                    thirdVertex * tile.lossyScale.x * 100f);
+                Vector3 up = plane.normal.normalized;
+                Vector3 forward = (mesh.vertices[0] * tile.lossyScale.x - center * tile.lossyScale.x).normalized;
+                Quaternion frameRotation = new Quaternion();
+                frameRotation.SetLookRotation(forward, -up);
+                frame.transform.rotation = frameRotation;
+
+                frame.transform.localScale = Vector3.one * scale;
+                frame.transform.parent = tile;
+            }
+        }
+    }
+}
+
